168e73500243704cc0833bde2af5e615f2e4eea5
Ammar Al-Khawaldeh <me@ammar.dev>
2023-11-21 20:24:13 +0300

    Format main.go

--- main.go
+++ main.go
@@ -9,45 +9,44 @@ import (
 )
 
 func main() {
-    listener, err := net.Listen("tcp", "0.0.0.0:9000");
-    if err != nil {
-        log.Fatalln(err)
-    }
+	listener, err := net.Listen("tcp", "0.0.0.0:9000")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    defer listener.Close()
+	defer listener.Close()
 
-    for {
-        connection, err := listener.Accept()
-        if err != nil {
-            log.Fatalln(err)
-        }
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-        go handleConnection(connection)
-    }
+		go handleConnection(connection)
+	}
 }
 
-
 func handleConnection(connection net.Conn) {
-    defer connection.Close()
+	defer connection.Close()
 
-    request, err := messages.NewRequest(connection)
-    if err != nil {
-        connection.Write([]byte(err.Error() + "\r\n"))
-        return
-    }
+	request, err := messages.NewRequest(connection)
+	if err != nil {
+		connection.Write([]byte(err.Error() + "\r\n"))
+		return
+	}
 
-    fileName := "index.html"
+	fileName := "index.html"
 
-    if request.URI != "/" {
-        fileName = request.URI
-    }
+	if request.URI != "/" {
+		fileName = request.URI
+	}
 
-    file, err := os.ReadFile("./public/" + fileName)
-    if err != nil {
-        connection.Write([]byte("HTTP/1.0 404 File Not Found\r\n"))
-        return
-    }
+	file, err := os.ReadFile("./public/" + fileName)
+	if err != nil {
+		connection.Write([]byte("HTTP/1.0 404 File Not Found\r\n"))
+		return
+	}
 
-    connection.Write([]byte("HTTP/1.0 200 Success\r\n\r\n"))
-    connection.Write(file)
+	connection.Write([]byte("HTTP/1.0 200 Success\r\n\r\n"))
+	connection.Write(file)
 }
